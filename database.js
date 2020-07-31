e2baa6494338d44994397d09cdac92b50abc73ca
KhanhNguyenKHTN <32816030+KhanhNguyenKHTN@users.noreply.github.com>
2020-07-31 19:16:44 +0700

    update code

---
+++ database.js
@@ -0,0 +1,63 @@
+var mysql = require('mysql');
+var lib = require('./lib');
+var crypto = require('crypto');
+
+var con = mysql.createConnection({
+    host: "35.196.95.74",
+    user: "khanhnguyen",
+    password: "khanhnguyen",
+    database: "SoundDb"
+  });
+  
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+function AddLineToLastId(link, callback)
+{
+  var sql = "SELECT Id FROM SoundInfo ORDER BY id DESC LIMIT 1;";
+  con.query(sql, function(err, results) {
+    console.log('Result: ', results);
+    if (results.length !==  1) {
+        console.error('not found sound ', gameId);
+        return callback('NO_GAME_HASH');
+    }
+
+    var lastId = results[0].Id;
+    var updateQuery = "UPDATE SoundInfo SET Link = '" + link + "' WHERE Id = " + lastId;
+    con.query(updateQuery);
+  });
+}
+
+exports.UpdateLink = AddLineToLastId;
+
+exports.updateDownLoad = function(url, callback)
+{
+    var sql = `SELECT Id, Link, KeySound, Direction FROM SoundInfo WHERE Link='${url}' ORDER BY id DESC LIMIT 1;`;
+    con.query(sql, function(err, results) {
+        // console.log('Result: ', results);
+        if(err) {
+            console.log(err);
+            return callback(err);
+        }
+        if (results.length !==  1) {
+            console.log('not found sound link:', url);
+            return callback('NO_GAME_HASH');
+        }
+        var lastId = results[0].Id;
+        var link = results[0].Link;
+        var keySound = results[0].KeySound;
+        var direction = results[0].Direction;
+        var description = lib.removeUniCode(keySound);
+        var hash = crypto.createHash('sha256').update(keySound).digest('hex');
+        var fileName = description + '.mp3';
+        var realPath =  direction + '/' + fileName;
+        var updateQuery = `UPDATE SoundInfo SET FileName = '${fileName}' , RealPath = '${realPath}', Hash ='${hash}', Description = '${description}'  WHERE Id = ${lastId}`;
+        con.query(updateQuery, function(nerr){
+            if(nerr) console.log(nerr);
+            console.log('Updated for: ', keySound);
+            callback(null, { path: realPath, direction: direction, fileName: fileName, url: link});
+        });
+      });
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require('http');
-
+var db = require('./database');
+var fs = require('fs');
+var lib = require('./lib');
 http.createServer(function (request, response) {
     const { headers, method, url } = request;
     let body = [];
@@ -9,11 +11,50 @@ http.createServer(function (request, response) {
       body.push(chunk);
     }).on('end', () => {
       body = Buffer.concat(body).toString();
-      console.log(request.headers);
-      console.log(body);
+      var res = Direction(request, body);
+      if(res == "")
+      {
+          response.writeHead(200, { 'Content-Type': 'text/plain' });
+          response.write('Hello World!');
+          response.end();
+      }else {
+          fs.readFile('./index.html', function (err, html) {
+              if (err) {
+                  throw err; 
+              }       
+              response.writeHeader(200, {"Content-Type": "text/html"});  
+              response.write(html);  
+              response.end(); 
+          });
+      }
     });
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World!');
-    response.end();
+   
 }).listen(8080);
+console.log("Server start in port 8080");
 
+function Direction(request, body) {
+    if(request.method == 'POST')
+    {
+        //call Back Function
+        var data = JSON.parse(body);
+        db.updateDownLoad(data.message, function(err, info){
+            if(err) {
+                console.log(err);
+                return;
+            }
+            lib.downloadAudio(info.direction, info.fileName, info.url);
+        });
+        return "";
+    }
+    else if (request.method == 'GET'){
+        //Redirect function
+        switch (request.url) {
+            case '/':               
+                break;
+            case '/home':
+                return "zz";
+            default:
+                break;
+        }
+    }
+}
